85f5143693e338da37935b45d73c08fc2a39537d
GuuhX23 <gu.paulella@gmail.com>
2026-08-14 22:59:39 -0300

    refactor(usuario): adiciona ResponseEntity e status HTTP corretos no UsuarioController

--- src/main/java/com/example/sistemadebiblioteca/sistema_biblioteca/controller/UsuarioController.java
+++ src/main/java/com/example/sistemadebiblioteca/sistema_biblioteca/controller/UsuarioController.java
@@ -4,6 +4,10 @@ package com.example.sistemadebiblioteca.sistema_biblioteca.controller;
 import com.example.sistemadebiblioteca.sistema_biblioteca.entities.Usuario;
 import com.example.sistemadebiblioteca.sistema_biblioteca.service.UsuarioService;
 import jakarta.persistence.NamedStoredProcedureQuery;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,22 +24,31 @@ public class UsuarioController {
     }
 
     @GetMapping("/{id}")
-    public Optional<Usuario> buscarId(@PathVariable Long id) {
-        return usuarioService.buscarId(id);
+    public ResponseEntity<Usuario> buscarId(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioService.buscarId(id);
+
+        if(usuario.isPresent()) {
+            return ResponseEntity.ok(usuario.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping
-    public List<Usuario> listarTodos() {
-        return usuarioService.listarTodos();
+    public ResponseEntity<List<Usuario>> listarTodos() {
+        List<Usuario> usuarios = usuarioService.listarTodos();
+        return ResponseEntity.ok(usuarios);
     }
 
     @PostMapping
-    public Usuario salvarUsuario(@RequestBody Usuario usuario) {
-        return usuarioService.salvarUsuario(usuario);
+    public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioSalvo = usuarioService.salvarUsuario(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioSalvo);
     }
 
     @DeleteMapping("/{id}")
-    public void deletar(@PathVariable Long id) {
-        usuarioService.deletar(id);
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+       usuarioService.deletar(id);
+       return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
