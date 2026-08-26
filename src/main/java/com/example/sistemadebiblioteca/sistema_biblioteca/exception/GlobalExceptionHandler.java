1aeb0d92da784e9ddc1ced1e4e19a7e6be9d2ef7
GuuhX23 <gu.paulella@gmail.com>
2026-08-26 16:53:15 -0300

    feat: adiciona validacao de dados com Bean Validation e tratamento de erros de validacao

--- src/main/java/com/example/sistemadebiblioteca/sistema_biblioteca/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/sistemadebiblioteca/sistema_biblioteca/exception/GlobalExceptionHandler.java
@@ -3,9 +3,14 @@ package com.example.sistemadebiblioteca.sistema_biblioteca.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
@@ -14,5 +19,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<String> tratarNaoEncontrado (NoSuchElementException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> tratarValidacao(MethodArgumentNotValidException e) {
+        Map<String, String> erros = new HashMap<>();
+
+        for (FieldError erro : e.getBindingResult().getFieldErrors()) {
+            erros.put(erro.getField(), erro.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros);
     }
 }
